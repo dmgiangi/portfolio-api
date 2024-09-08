8643b69ed18cb5572e98b80e06a39517740b1890
Gianluigi De Marco <dem.gianluigi@gmail.com>
2024-09-09 00:06:14 +0200

    feat: use brevo as mail provider

--- function_app.py
+++ function_app.py
@@ -1,26 +1,52 @@
+import logging as logger
+import os
+import requests
 import azure.functions as func
-import logging
 
 app = func.FunctionApp()
 
 
 @app.route(route="mail", methods=[func.HttpMethod.POST], auth_level=func.AuthLevel.ANONYMOUS)
 def mail_sender(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+    try:
+        req_body = req.get_json()
+        name = req_body.get('name')
+        email = req_body.get('email')
+        message = req_body.get('message')
+    except ValueError:
         return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200
+            "Invalid input. Make sure to provide 'name', 'email', and 'message'.",
+            status_code=400
         )
+
+    brevo_api_key = os.getenv("BREVO_API_KEY")
+    brevo_url = "https://api.brevo.com/v3/smtp/email"
+
+    # Email content
+    email_data = {
+        "sender": {
+            "name": f"{name}",
+            "email": f"{email}"
+        },
+        "to": [
+            {
+                "name": "Gianluigi De Marco",
+                "email": "dem.gianluigi@gmail.com"
+            }
+        ],
+        "subject": "Message from your portfolio's contact form",
+        "htmlContent": f"<html><head></head><body><p><strong>Name:</strong> {name}</p><p><strong>Email:</strong> {email}</p><p><strong>Message:</strong> {message}</p></body></html>"
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "api-key": brevo_api_key
+    }
+
+    response = requests.post(brevo_url, json=email_data, headers=headers)
+
+    if response.status_code == 201:
+        return func.HttpResponse(f"Email successfully sent.", status_code=201)
+    else:
+        logger.log(20, f"Error sending the mail: {response.text}")
+        return func.HttpResponse(f"Failed to send email.", status_code=500)
